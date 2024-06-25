749dd12fb95a4a027b0c5b2358b18ef41844a031
rodrigoaslima <rodrigoaslima@hotmail.com>
2024-06-25 09:42:31 -0300

    adicionando pagina no github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
+import { ghPages } from 'vite-plugin-gh-pages'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),ghPages()],
 })
